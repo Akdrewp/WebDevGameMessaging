5780c2d6ed986371b302051c553cadbd51a3173f
James Paul <jamesterpaul@gmail.com>
2023-05-08 18:22:04 -0400

    Added the frontend

---
+++ Game-Messaging/reactbrontend/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ChatPage from './Pages/ChatPage';
+import LoginPage from './Pages/LoginPage';
+import { Routes, Route } from "react-router-dom";
+
+
+
+// const socketIO = io.connect("http://localhost:8080");
+
+// socketIO.on("connect", () => {
+//   console.log("Connected");
+// });
+
+// function sendMessage() {
+//   socketIO.emit("send_message", { data: "Hello!"} );
+// }
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h1 className="border-bottom border-2">Chess web and messaging App</h1>
+        </div>
+      </header>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/messages" element={<ChatPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ Game-Messaging/reactbrontend/src/Pages/LoginPage.js
@@ -0,0 +1,36 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from "react-bootstrap/Button";
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form';
+
+function LoginPage() {
+    return (
+        <Container fluid className="main-content">
+            <Row>
+                <Col className="flex-column">
+                    <div className="user-form-container flex-grow">
+                        <Form className="user-form border border-1 border-dark">
+                            <p className="border-bottom border-2">Login</p>
+                            <Form.Group className="mb-3" controlId="userNameInput">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control type="text" placeholder="Enter username" />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="passwordInput">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default LoginPage;
